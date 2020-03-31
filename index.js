fb3d53f5005b695ffec651bde7298a27ed0faa12
zerofaction <erin.wallaard@outlook.com>
2020-03-31 10:28:34 +0200

    Sicko Message

--- index.js
+++ index.js
@@ -4,36 +4,30 @@ const fs = require('fs');
 const { Recognizer } = require('node-nlp');
 const modelName = './smalltalk.nlp';
 const excelName = './smalltalk.xls';
+var http = require('http');
+const { NlpManager } = require('node-nlp');
+
 
 // Creates a connector for the chatbot
 const connector = new builder.ChatConnector({
-    appId: process.env.BOT_APP_ID,
-    appPassword: process.env.BOT_APP_PASSWORD,
+    appId: null,
+    appPassword: null,
 });
 
 // Creates a node-nlp recognizer for the bot
-const recognizer = new Recognizer();
-if (fs.existsSync(modelName)) {
-    recognizer.load(modelName);
-} else {
-    recognizer.loadExcel(excelName);
-    recognizer.save(modelName);
-}
-
-// Creates the bot using a memory storage, with a main dialog that
-// use the node-nlp recognizer to calculate the answer.
-const bot = new builder.UniversalBot(connector, (session) => {
-    recognizer.recognize(session, (err, data) => {
-        session.send(data.answer || 'I don\'t understand');
-        session.endDialog();
-    });
-}).set('storage', new builder.MemoryBotStorage());
-
-recognizer.setBot(bot, false);
+const manager = new NlpManager({ languages: ['en'] });
+manager.load(modelName);
 
 // Creates the express application
 const app = express();
 const port = process.env.PORT || 3000;
 app.post('/api/messages', connector.listen());
+
+app.get('/test/:message', async function (req, res, next) {
+    const message = req.params.message;
+    manager.process(message).then(result =>
+        res.status(200).json(result.answer));
+})
+
 app.listen(port);
-console.log(`Chatbot listening on port ${port}`);
+console.log('Chatbot listening on port 3000');
